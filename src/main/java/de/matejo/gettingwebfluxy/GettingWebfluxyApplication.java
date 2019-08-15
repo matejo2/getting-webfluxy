a0333b2f2f2cb32d0717678de4bfbbaf208f9211
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2019-08-15 21:43:31 +0200

    initial commit

---
+++ src/main/java/de/matejo/gettingwebfluxy/GettingWebfluxyApplication.java
@@ -0,0 +1,13 @@
+package de.matejo.gettingwebfluxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingWebfluxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingWebfluxyApplication.class, args);
+	}
+
+}
